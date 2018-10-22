f081568eef5bb69d1b6bf6908824ef3b352db4f0
rvikwd3 <rvikwd3@outlook.com>
2018-10-22 18:03:05 +0530

    Put best bitrates into functions Added origami fold markers

--- ytdl-script.sh
+++ ytdl-script.sh
@@ -16,35 +16,11 @@
 ##		4.	Call mpv with format options
 ##		5.	Call 'mpv --ytdl-format=video+audio'
 ##		6.	Include video container format
+##		7.	Parameterize "best" functions
 ##
 ######################################################################################
 
-# OLD GETOPTS
-# -----------
-# Check for option flags
-#while getopts ":v:a:u:" opt; do
-#	case $opt in
-#		v)
-#				echo "-v was triggered	Parameter: $OPTARG" >&2
-#				;;
-#		a)
-#				echo "-a was triggered	Parameter: $OPTARG" >&2
-#				;;
-#		u)
-#				echo "-u was triggered	Parameter: $OPTARG" >&2
-#				;;
-#		\?)
-#				echo "Invalid option: -$OPTARG" >&2
-#				exit 1
-#				;;
-#		:)
-#				echo "Option -$OPTARG requires an argument." >&2
-#				exit 1
-#				;;
-#		esac
-#done
-
-# NEW GETOPT
+# NEW GETOPT									 {{{
 # ----------
 OPTIONS=v:a:u:h		# -v video -a audio -u URL -h help
 LONGOPTIONS=video:,audio:,url:,help
@@ -101,16 +77,16 @@ while true; do
 			break
 			;;
 		*)
-			echo "Internal error!\tERROR_FLAG_CASE *"
+			echo "Internal error!	ERROR_FLAG_CASE *"
 			exit 1
 			;;
 	esac
-done
+done	#}}}
 
 # Functions
 # ---------
 # Make sure to trash temp file on crash
-cleanup(){
+cleanup(){									#{{{
 	printf "\nCleanup...\n"
 	if [[ -f $ytdl_tmpfile ]]; then
 		rm $ytdl_tmpfile -f
@@ -118,108 +94,42 @@ cleanup(){
 	fi
 	printf "\nFinished cleanup\n"
 	exit 1
-}
+}	#}}}
+
+# Select best video format from youtube-dl
+best_video(){									#{{{
+	bitrate=($(grep -E video < $ytdl_tmpfile | grep -Eo '[0-9]{3,4}p' | sed 's/[^0-9p]//g' ))
+
+	# print bitrates before sorting
+	echo "[VIDEO] Before sort"
 
 
-# Parameter variables
-echo "Video:	$VIDEO"
-echo "Audio:	$AUDIO"
-echo "URL:	$URL"
-
-# Arguments
-args=()
-while [[ $# -gt 0 ]]
-do
-	arg="$1"
-	shift
-
-	echo $arg
-done
-
-# Create temporary file for ytdl format lines
-# -------------------------------------------
-ytdl_tmpfile=$(mktemp /tmp/ytdl-script.XXXXX)
-
-# Gracefully exit on crash
-trap cleanup 0 2 3 15
-
-# Populate tempfile
-echo "Tempfile made:	$ytdl_tmpfile"
-youtube-dl -F "$URL" > $ytdl_tmpfile
-echo "Tempfile populated"
-
-# Check if video, audio, URL are set
-if [[ -z ${URL+x} ]]; then
-	echo "-----------------------------------------------"
-	echo "URL unset"
-	read -p "Enter URL:	" URL
-	echo "URL:	$URL"
-fi
-
-# Set video resolution
-# --------------------
-while [[ -z ${VIDEO+x} ]]
-do
-	echo "-----------------------------------------------"
-	echo "Resolution unset"
-
-	# Since URL is definitely set,
-	# list youtube-dl formats
-
-	printf '\nVideo Resolutions:\n'
-	echo "------------------"
-
-	#for line in $(cat "$ytdl_tmpfile")
-	while read line;
+	for i in "${bitrate[@]}"
 	do
-		echo "$line" | sed -n '/video/p' | awk '{ print $2"\t"$4 }' | sed -e '/video/d'
-	done < $ytdl_tmpfile
+		printf "[BITRATE - $i] ${bitrate[$i]}"
+		echo "[VIDEO] bitrate - " $i
+	done
 
-	read -p "Enter video resolution:	" VIDEO
-	echo "VIDEO:	$VIDEO"
+	# sort video bitrates
+	echo "[VIDEO] Sort bitrates"
+	bitrate_sorted=( $(
+		for i in "${bitrate[@]}"
+		do
+			echo "$i" | sed "s/k//"
+		done | sort -gr) )
 
-	if ! [[ $VIDEO =~ ^[0-9]{3,4}p$  ||  $VIDEO =~ ^[0-9]{3,4}$  ]]; then
-		echo "Not a valid VIDEO resolution"
-		unset VIDEO
-	fi
-done
-
-# Set audio resolution
-# --------------------
-if [[ -z ${AUDIO+x} ]]; then
-	echo "-----------------------------------------------"
-	echo "Audio unset"
-
-	# Since URL is definitely set,
-	# list youtube-dl formats
-
-	printf '\nAudio Resolutions:\n'
-	echo "------------------"
-
-	#for line in $(cat "$ytdl_tmpfile")
-	while read line;
+	# print bitrates after sorting
+	echo "[VIDEO] After sort"
+	for i in "${bitrate_sorted[@]}"
 	do
-		echo "$line" | sed -n '/audio/p' | awk '{ print $2"\t"$7 }'
-	done < $ytdl_tmpfile
+		echo $i
+	done
 
-	read -p "Enter audio quality:	" AUDIO
-	echo "AUDIO:	$AUDIO"
-fi
+	VIDEO="${bitrate_sorted[0]}"
+}	#}}}
 
-# Find format index from ytdl -F
-# ------------------------------
-if [[ "$VIDEO" -eq "best" ]]
-then
-	echo "[VIDEO] BEST"
-	# compare video qualities
-else
-	VIDEO_FORMAT="$(grep -E video < $ytdl_tmpfile | grep -E $VIDEO | head -n 1 | awk '{ print $1 }')"
-	echo "Video format = $VIDEO_FORMAT"
-fi
-
-if [[ "$AUDIO" -eq "best" ]]
-then
-	echo "[AUDIO] BEST"
+# Select best audio format from youtube-dl
+best_audio(){									#{{{
 	bitrate=($(grep -E audio < $ytdl_tmpfile | grep -Eo '@[0-9]{2,3}k|@ [0-9]{2,3}k' | sed 's/[^0-9k]//g' ))
 
 	# print bitrates before sorting
@@ -245,15 +155,133 @@ then
 		echo $i
 	done
 
-	AUDIO=${bitrate_sorted[0]}
+	AUDIO="${bitrate_sorted[0]}"
+}	#}}}
+
+# Parameter variables
+echo "Video:	$VIDEO"
+echo "Audio:	$AUDIO"
+echo "URL:	$URL"
+
+# Arguments
+args=()
+while [[ $# -gt 0 ]]
+do
+	arg="$1"
+	shift
+
+	echo $arg
+done
+
+>&2 echo "[DEBUG] Parameter variables declared"
+
+# Create temporary file for ytdl format lines
+# -------------------------------------------
+ytdl_tmpfile=$(mktemp /tmp/ytdl-script.XXXXX)
+
+# Gracefully exit on crash
+trap cleanup 0 2 3 15
+
+# Populate tempfile
+echo "Tempfile made:	$ytdl_tmpfile"
+youtube-dl -F "$URL" > $ytdl_tmpfile
+echo "Tempfile populated"
+
+# Check if video, audio, URL are set
+if [[ -z ${URL+x} ]]; then
+	echo "-----------------------------------------------"
+	echo "URL unset"
+	read -p "Enter URL:	" URL
+	echo "URL:	$URL"
+fi
+
+>&2 echo "[DEBUG] Video, Audio, URL set & Tempfile populated"
+
+# Set video resolution
+# --------------------
+while [[ -z ${VIDEO+x} ]]						#{{{
+do
+	echo "-----------------------------------------------"
+	echo "Resolution unset"
+
+	# Since URL is definitely set,
+	# list youtube-dl formats
+
+	printf '\nVideo Resolutions:\n'
+	echo "------------------"
+
+	#for line in $(cat "$ytdl_tmpfile")
+	while read line;
+	do
+		echo "$line" | sed -n '/video/p' | awk '{ print $2"	"$4 }' | sed -e '/video/d'
+	done < $ytdl_tmpfile
+
+	read -p "Enter video resolution:	" VIDEO
+	echo "VIDEO:	$VIDEO"
+
+	if ! [[ $VIDEO =~ ^[0-9]{3,4}p$  ||  $VIDEO =~ ^[0-9]{3,4}$  ]]; then
+		echo "Not a valid VIDEO resolution"
+		unset VIDEO
+	fi
+done	#}}}
+
+>&2 echo "[DEBUG] Video resolution set"
+
+# Set audio resolution
+# --------------------
+if [[ -z ${AUDIO+x} ]]; then					#{{{
+	echo "-----------------------------------------------"
+	echo "Audio unset"
+
+	# Since URL is definitely set,
+	# list youtube-dl formats
+
+	printf '\nAudio Resolutions:\n'
+	echo "------------------"
+
+	#for line in $(cat "$ytdl_tmpfile")
+	while read line;
+	do
+		echo "$line" | sed -n '/audio/p' | awk '{ print $2"	"$7 }'
+	done < $ytdl_tmpfile
+
+	read -p "Enter audio quality:	" AUDIO
+	echo "AUDIO:	$AUDIO"
+fi	#}}}
+
+>&2 echo "[DEBUG] Audio resolution set"
+
+# Find format index from ytdl -F
+# ------------------------------
+
+# if quality is best, find the best bitrate	#{{{
+if [[ "$VIDEO" -eq "best" ]]; then
+	echo "[VIDEO] BEST"
+	# compare video qualities
+
+	best_video()
+	echo "[VIDEO] " $VIDEO
+fi
+
+if [[ "$AUDIO" -eq "best" ]]; then
+	echo "[AUDIO] BEST"
+	# compare audio qualities
+
+	best_audio()
 	echo "[AUDIO] " $AUDIO
 
-fi
+fi	#}}}
+
+>&2 echo "[DEBUG] Past best bitrate"
 
 AUDIO_FORMAT="$(grep -E audio < $ytdl_tmpfile | grep -E $AUDIO"k" | head -n 1 | awk '{ print $1 }')"
 echo "Audio format = $AUDIO_FORMAT"
+
+VIDEO_FORMAT="$(grep -E video < $ytdl_tmpfile | grep -E $VIDEO | head -n 1 | awk '{ print $1 }')"
+echo "Video format = $VIDEO_FORMAT"
 
 
 rm "$ytdl_tmpfile"
 
 ! echo "Complete"
+
